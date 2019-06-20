ac1012d48cc55d05d8e3b1eece46da4c06caec0c
gsellitto <gianluigi@sellitto.it>
2019-06-20 11:54:10 +0200

    ok

--- SqlResourceHelper.cs
+++ SqlResourceHelper.cs
@@ -64,6 +64,7 @@ namespace SqlResourcesNameSpace
         /// riga da non portare
         /// seconda riga
         /// terza riga
+        /// quarta riga
         /// <summary>
         /// Legge i valori dal db. VirtualPath è valorizzato nel caso di risorse locali, classnme è valorizzato nel caso di risorse globali.
         /// sono mutualmente esclusivi e vanno nella stessa colonna resource_object del db. Hanno peró due siginificati diversi.
